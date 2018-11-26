2b03577f86609a9109885f05890452ebef81015c
CodeWarrior1241 <CodeWarrior1241@users.noreply.github.com>
2018-11-26 18:02:46 -0500

    Added some detail on how to convert hex to Matlab signed FP.

--- ILA_data_to_mat.m
+++ ILA_data_to_mat.m
@@ -9,6 +9,7 @@ Q_data = zeros(1,128);
 I_data(1:2:end) = hex2dec(hex_data(:,1:4));
 I_data(2:2:end) = hex2dec(hex_data(:,9:12));
 
-% Scale and produce the 16,15 signed fixed-point int, that still needs to be converted to matlab fp 
+% Scale and produce the 16,15 signed fixed-point int, that still needs to be converted to matlab fp
+% To convert hex to signed fp example: q = quantizer([16 15]); h = '00B6'; x = hex2num(q,h);
 I_data_scaled = (I_data - (2^15 - 1));
 I_data_fp = I_data_scaled/max(abs(I_data_scaled));
